10c82b5a1483837fac8d2cc1ba9cff443671bebd
vniroshan <nnirosh447@gmail.com>
2024-10-18 21:12:18 +0530

    sms otp added

--- server/plugins/otp.js
+++ server/plugins/otp.js
@@ -13,14 +13,17 @@ module.exports = fp(async function (fastify, opts) {
       return otp;
     },
     send: async (currentFastify, params, code) => {
-      var message =
-        "Yappzy: Your OTP is " +
-        code +
-        ". Only valid for 5 mins.";
+        const message = `Yappzy: Your OTP is ${code}. Only valid for 5 mins.`;
 
-       let sms = {phone_number:params.phone_number, email: params.email,messaging_option:params.messaging_option,subject:`OTP`,message:message}
-      await fastify.sms.send(fastify, sms);
-      return { message: "Success!" };
+        const sms = {
+          phone_number: params.phone_number,
+          email: params.email,
+          messaging_option: params.messaging_option,
+          subject: "OTP",
+          message: message,
+        };
+        let sendSms = await fastify.sms.send(fastify, sms);
+        return sendSms;
     },
   };
   fastify.decorate("otp", otp);
